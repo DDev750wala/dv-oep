3fc6328c6eaf0e8170d1db4dfa09c0e1ef04a9f3
meet-panwala <meetpanwala200420@gmail.com>
2025-03-27 11:09:25 +0530

    temp

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
+import Papa from "papaparse";
+import csvData from "../assets/data.csv?raw";
 
 function App() {
+    const parsed = Papa.parse(csvData, { header: true });
+    console.log(parsed);
+    
 
-  return (
-    <>
-      <h1 className='text-5xl font-extrabold'>Sagrampura</h1>
-      <Button className='text-black' variant={'default'}>click to propose sagrampura</Button>
-    </>
-  )
+    return (
+        <>
+            <h1 className="text-5xl font-extrabold">Sagrampura</h1>
+            <Button className="text-black" variant={"default"}>
+                click to propose sagrampura
+            </Button>
+        </>
+    );
 }
 
-export default App
+export default App;

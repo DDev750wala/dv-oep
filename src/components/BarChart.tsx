808300e0bfdf3114672dc982d5dd208120330b40
meet-panwala <meetpanwala200420@gmail.com>
2025-04-02 12:29:53 +0530

    temp

--- src/components/BarChart.tsx
+++ src/components/BarChart.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {Bar} from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
 
 const BarChart = () => {
-  return (
-    <div>BarChart</div>
-  )
+  const data = {
+
+  }
+
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: { position: "top" },
+      title: { display: true, text: "Sample Bar Chart" },
+    },
+  };
+
+ return <Bar data={data} options={options} />;
 }
 
 export default BarChart
---
+++ src/components/ScatterChart.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import {Scatter} from "react-chartjs-2";
+import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, Title, Tooltip, Legend } from "chart.js";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, Title, Tooltip, Legend);
+

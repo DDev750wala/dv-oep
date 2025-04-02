1dd1e00aa4da221a1cbdd755f79893f48fef103f
dev750wala <sadisatsowaladev1@gmail.com>
2025-04-02 13:23:33 +0530

    Update title in index.html and remove unused imports in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useContext, useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import Papa from "papaparse";
